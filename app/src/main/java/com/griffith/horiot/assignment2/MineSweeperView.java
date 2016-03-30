aa616c68d9ef74eebcdfe8a54e57b8e3ebf4d6bd
Benjamin Horiot <benjamin.horiot@epitech.eu>
2016-03-30 17:41:51 +0200

    number in each case (Milestone 6)

--- app/src/main/java/com/griffith/horiot/assignment2/MineSweeperView.java
+++ app/src/main/java/com/griffith/horiot/assignment2/MineSweeperView.java
@@ -3,7 +3,6 @@ package com.griffith.horiot.assignment2;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -16,8 +15,7 @@ import java.util.Random;
  */
 public class MineSweeperView extends View {
 
-    private Paint black, grey, red;
-    private Rect square; // the square itself
+    private Paint black, grey, red, green, blue, yellow;
     private int gameSquare[][];
     private int statusSquare[][];
     private int size;
@@ -25,13 +23,13 @@ public class MineSweeperView extends View {
 
     /*
     Legend gameSquare
-    -1 = uncovered
     0 = void
-    1 - 9 = nbr mines proxy
+    1 - 8 = nbr mines proxy
+    >= 9 = mine
 
     Legend statusSquare
-    -1 = mine
-    0 = void
+    -1 = discovered
+    0 = covered
     1 = marked
      */
 
@@ -60,23 +58,26 @@ public class MineSweeperView extends View {
     // constructors
     private void init() {
 
-        // initialise the rectangle
-        square = new Rect(-100, -100, 100, 100);
-
         black = new Paint();
         grey = new Paint();
         red = new Paint();
+        green = new Paint();
+        blue = new Paint();
+        yellow = new Paint();
 
         black.setColor(getResources().getColor(R.color.black));
         grey.setColor(getResources().getColor(R.color.grey));
         red.setColor(getResources().getColor(R.color.red));
+        green.setColor(getResources().getColor(R.color.green));
+        blue.setColor(getResources().getColor(R.color.blue));
+        yellow.setColor(getResources().getColor(R.color.yellow));
 
         gameSquare = new int[10][10];
         statusSquare = new int[10][10];
 
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
-                gameSquare[x][y] = -1;
+                gameSquare[x][y] = 0;
                 statusSquare[x][y] = 0;
             }
         }
@@ -88,8 +89,23 @@ public class MineSweeperView extends View {
             x = random.nextInt(10);
             y = random.nextInt(10);
 
-            if (statusSquare[x][y] == 0) {
-                statusSquare[x][y] = -1;
+            if (gameSquare[x][y] < 9) {
+                gameSquare[x][y] = 9;
+
+                if (x > 0) {
+                    gameSquare[x - 1][y] += 1;
+                    if (y > 0) gameSquare[x - 1][y - 1] += 1;
+                    if (y < 9) gameSquare[x - 1][y + 1] += 1;
+                }
+                if (x < 9) {
+                    gameSquare[x + 1][y] += 1;
+                    if (y > 0) gameSquare[x + 1][y - 1] += 1;
+                    if (y < 9) gameSquare[x + 1][y + 1] += 1;
+                }
+
+                if (y > 0) gameSquare[x][y - 1] += 1;
+                if (y < 9) gameSquare[x][y + 1] += 1;
+
                 nbrMinesPlaces++;
             }
         }
@@ -107,20 +123,27 @@ public class MineSweeperView extends View {
                 rect.set(x * (size / 10),  y * (size / 10),
                         (x + 1) * (size / 10) - 5, (y + 1) * (size / 10) - 5);
 
-                if (gameSquare[x][y] == -1) {
+                if (statusSquare[x][y] == 0) {
                     canvas.save();
                     canvas.drawRect(rect, black);
                     canvas.restore();
 
-                } else if (gameSquare[x][y] == 0) {
+                } else {
                     canvas.save();
-                    if (statusSquare[x][y] == 0) {
-                        canvas.drawRect(rect, grey);
-                    } else if (statusSquare[x][y] == -1) {
-                        black.setTextSize(size / 10);
-
+                    if (gameSquare[x][y] >= 9) {
                         canvas.drawRect(rect, red);
                         canvas.drawText("M", x * (size / 10) + 5, (y + 1) * (size / 10) - 15, black);
+                    } else {
+                        canvas.drawRect(rect, grey);
+                        if (gameSquare[x][y] == 1) {
+                            canvas.drawText("1", x * (size / 10) + 5, (y + 1) * (size / 10) - 15, blue);
+                        } else if (gameSquare[x][y] == 2) {
+                            canvas.drawText("2", x * (size / 10) + 5, (y + 1) * (size / 10) - 15, green);
+                        } else if (gameSquare[x][y] == 3) {
+                            canvas.drawText("3", x * (size / 10) + 5, (y + 1) * (size / 10) - 15, yellow);
+                        } else if (gameSquare[x][y] > 3) {
+                            canvas.drawText(Integer.toString(gameSquare[x][y]), x * (size / 10) + 5, (y + 1) * (size / 10) - 15, red);
+                        }
                     }
                     canvas.restore();
 
@@ -140,6 +163,12 @@ public class MineSweeperView extends View {
 
         size = width > height ? height : width;
         setMeasuredDimension(size, size);
+
+        black.setTextSize(size / 10);
+        red.setTextSize(size / 10);
+        green.setTextSize(size / 10);
+        blue.setTextSize(size / 10);
+        yellow.setTextSize(size / 10);
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -155,7 +184,7 @@ public class MineSweeperView extends View {
             x = (int)event.getX() / (size / 10);
             y = (int)event.getY() / (size / 10);
 
-            this.gameSquare[x][y] = 0;
+            statusSquare[x][y] = -1;
 
             invalidate();
             return true;
