dba0456449779942db58f61d93a49e442f2c2013
Benjamin Horiot <benjamin.horiot@epitech.eu>
2016-03-30 16:37:41 +0200

    touch on grid changes the color of the case (Milestone 4)

--- app/src/main/java/com/griffith/horiot/assignment2/MineSweeperView.java
+++ app/src/main/java/com/griffith/horiot/assignment2/MineSweeperView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -12,7 +13,7 @@ import android.view.View;
  */
 public class MineSweeperView extends View {
 
-    private Paint black;
+    private Paint black, grey;
     private Rect square; // the square itself
     private int gameSquare[][];
     private int statusSquare[][];
@@ -59,7 +60,11 @@ public class MineSweeperView extends View {
         // initialise the rectangle
         square = new Rect(-100, -100, 100, 100);
 
-        black = new Paint(R.color.black);
+        black = new Paint();
+        grey = new Paint();
+
+        black.setColor(getResources().getColor(R.color.black));
+        grey.setColor(getResources().getColor(R.color.grey));
 
         gameSquare = new int[10][10];
         statusSquare = new int[10][10];
@@ -76,9 +81,6 @@ public class MineSweeperView extends View {
     // public method that needs to be overridden to draw the contents of this
     // widget
     public void onDraw(Canvas canvas) {
-        // call the superclass method
-        super.onDraw(canvas);
-
         // draw the rest of the squares in green to indicate multitouch
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
@@ -91,9 +93,21 @@ public class MineSweeperView extends View {
                     //canvas.drawRect(square, black);
                     canvas.restore();
 
+                } else if (gameSquare[x][y] == 0) {
+                    //draw black box
+                    canvas.save();
+                    canvas.drawRect(x * (size / 10), y * (size / 10),
+                            (x + 1) * (size / 10) - 5, (y + 1) * (size / 10) - 5,
+                            grey);
+                    //canvas.drawRect(square, black);
+                    canvas.restore();
+
                 }
             }
         }
+
+        // call the superclass method
+        super.onDraw(canvas);
 
     }
 
@@ -104,4 +118,40 @@ public class MineSweeperView extends View {
         size = width > height ? height : width;
         setMeasuredDimension(size, size);
     }
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int x, y;
+
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            // this indicates that the user has placed the first finger on the
+            // screen what we will do here is enable the pointer, track its location
+            // and indicate that the user is touching the screen right now
+            // we also take a copy of the pointer id as the initial pointer for this
+            // touch
+            int pointer_id = event.getPointerId(event.getActionIndex());
+
+            x = (int)event.getX() / (size / 10);
+            y = (int)event.getY() / (size / 10);
+
+            this.gameSquare[x][y] = 0;
+
+            invalidate();
+            return true;
+        } else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+            // this indicates that the user has removed the last finger from the
+            // screen and has ended all touch events. here we just disable the
+            // last touch.
+            int pointer_id = event.getPointerId(event.getActionIndex());
+
+            /*x = (int)event.getRawX() / (size / 10);
+            y = (int)event.getRawY() / (size / 10);*/
+
+            invalidate();
+            return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
 }
