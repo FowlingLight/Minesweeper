e1665a66bba3f2f72a2f87684f2ebc2eb67bc32f
Benjamin Horiot <benjamin.horiot@epitech.eu>
2016-03-30 15:27:00 +0200

    square view (Milestone 2)

--- app/src/main/java/com/griffith/horiot/assignment2/MineSweeperView.java
+++ app/src/main/java/com/griffith/horiot/assignment2/MineSweeperView.java
@@ -1,15 +1,14 @@
 package com.griffith.horiot.assignment2;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by horiot_b_to_chage on 30/03/2016 for Code and Learn
  */
 public class MineSweeperView extends View {
+
     // default constructor for the class that takes in a context
     public MineSweeperView(Context c) {
         super(c);
@@ -33,20 +32,13 @@ public class MineSweeperView extends View {
     // refactored init method as most of this code is shared by all the
     // constructors
     private void init() {
+
     }
 
-    // public method that needs to be overridden to draw the contents of this
-    // widget
-    public void onDraw(Canvas canvas) {
-        // call the superclass method
-        super.onDraw(canvas);
-    }
-
-    // public method that needs to be overridden to handle the touches from a
-    // user
-    public boolean onTouchEvent(MotionEvent event) {
-        // if we get to this point they we have not handled the touch
-        // ask the system to handle it instead
-        return super.onTouchEvent(event);
+    @Override public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int size = width > height ? height : width;
+        setMeasuredDimension(size, size);
     }
 }
